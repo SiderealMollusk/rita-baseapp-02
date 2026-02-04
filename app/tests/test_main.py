12ee2fec73351472b2731ff517c8e83a43d34c1b
Virgil <virgil@Virgils-MacBook-Pro.local>
2026-02-04 15:40:42 -0800

    test: update tests to match new hello message

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -10,13 +10,13 @@ def test_read_hello_default():
         del os.environ["APP_VERSION"]
     response = client.get("/hello")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello from version unknown"}
+    assert response.json() == {"message": "Hello, user! from version unknown"}
 
 def test_read_hello_with_version():
     os.environ["APP_VERSION"] = "1.2.3"
     response = client.get("/hello")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello from version 1.2.3"}
+    assert response.json() == {"message": "Hello, user! from version 1.2.3"}
 
 def test_health_check():
     response = client.get("/health")
